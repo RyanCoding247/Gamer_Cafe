7fda81e9455be6cad844f0ee47f0ae94fe694506
Ryan Nichols <kastrom247@gmail.com>
2022-12-19 22:53:46 -0600

    Finished Events Page

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
-$(function() {
-    $("#games1").click(function() {
+$(function () {
+    $("#games1").click(function () {
         $("#gameCarousel").carousel(0);
     });
-    $("#games2").click(function() {
+    $("#games2").click(function () {
         $("#gameCarousel").carousel(1);
     });
-    $("#games3").click(function() {
+    $("#games3").click(function () {
         $("#gameCarousel").carousel(2);
     });
-    $("#showGF").click(function() {
+    $("#showGF").click(function () {
         $(".foodItem").children("span").removeClass("highlightDF highlightSF");
         $(".gf").addClass("highlightGF");
     });
-    $("#showSF").click(function() {
+    $("#showSF").click(function () {
         $(".foodItem").children("span").removeClass("highlightDF highlightGF");
         $(".sf").addClass("highlightSF");
     });
-    $("#showDF").click(function() {
+    $("#showDF").click(function () {
         $(".foodItem").children("span").removeClass("highlightGF highlightSF");
         $(".df").addClass("highlightDF");
     });
     $("[data-toggle='tooltip']").tooltip();
+    $("#vrButton").click(function () {
+        $("#roomModal").modal("toggle");
+        $(".eventMI").html("VR Sandbox");
+        $("#eventName").attr("placeholder", "VR Sandbox");
+    });
+    $("#drButton").click(function () {
+        $("#roomModal").modal("toggle");
+        $(".eventMI").html("D&D Room");
+        $("#eventName").attr("placeholder", "D&D Room");
+    });
+    $("#krButton").click(function () {
+        $("#roomModal").modal("toggle");
+        $(".eventMI").html("Karaoke Room");
+        $("#eventName").attr("placeholder", "Karaoke Room");
+    });
 });
